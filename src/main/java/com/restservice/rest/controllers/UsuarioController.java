fff7ef17019d85e69490133661b5585f40a98bd0
Tobias <tobiasocchi03@hotmail.com>
2023-02-21 14:18:43 +0100

    first commit

---
+++ src/main/java/com/restservice/rest/controllers/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.restservice.rest.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+    @RequestMapping(value = "prueba")
+    public String prueba() {
+        return "prueba";
+    }
+}
