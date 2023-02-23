bdd7ab42c421d9ec6f288c80c35fcb93b4b2ca07
Tobias <tobiasocchi03@hotmail.com>
2023-02-23 20:36:22 -0300

    added controllers

--- src/main/java/com/restservice/rest/controllers/UsuarioController.java
+++ src/main/java/com/restservice/rest/controllers/UsuarioController.java
@@ -1,12 +1,50 @@
 package com.restservice.rest.controllers;
 
+import com.restservice.rest.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public String prueba() {
-        return "prueba";
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario( @PathVariable Long id ) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("toto");
+        usuario.setApellido("occhiuzzi");
+        usuario.setEmail("tobisape5@gmail.com");
+        usuario.setTelefono("234234243");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario7")
+    public Usuario editar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("toto");
+        usuario.setApellido("occhiuzzi");
+        usuario.setEmail("tobisape5@gmail.com");
+        usuario.setTelefono("234234243");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario78")
+    public Usuario eliminar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("toto");
+        usuario.setApellido("occhiuzzi");
+        usuario.setEmail("tobisape5@gmail.com");
+        usuario.setTelefono("234234243");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario789")
+    public Usuario buscar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("toto");
+        usuario.setApellido("occhiuzzi");
+        usuario.setEmail("tobisape5@gmail.com");
+        usuario.setTelefono("234234243");
+        return usuario;
     }
 }
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,4 +1,7 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+
+    alert(3434);
+
+  $('#usuarios').DataTable();
 });
