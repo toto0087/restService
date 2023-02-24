5aab8680bb15fa36a25667fd4d200598296df362
Tobias <tobiasocchi03@hotmail.com>
2023-02-24 16:58:50 -0300

    added fetch with list

--- src/main/java/com/restservice/rest/controllers/UsuarioController.java
+++ src/main/java/com/restservice/rest/controllers/UsuarioController.java
@@ -18,6 +18,32 @@ public class UsuarioController {
         return usuario;
     }
 
+    @RequestMapping(value = "usuario")
+    public List<Usuario> getUsuario( @PathVariable Long id ) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("toto");
+        usuario.setApellido("occhiuzzi");
+        usuario.setEmail("tobisape5@gmail.com");
+        usuario.setTelefono("234234243");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(id);
+        usuario2.setNombre("toto2");
+        usuario2.setApellido("occhiuzzi2");
+        usuario2.setEmail("tobisape52@gmail.com");
+        usuario2.setTelefono("234234243");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(id);
+        usuario3.setNombre("toto3");
+        usuario3.setApellido("occhiuzzi3");
+        usuario3.setEmail("tobisape53@gmail.com");
+        usuario3.setTelefono("234234243");
+
+        return usuario;
+    }
+
     @RequestMapping(value = "usuario7")
     public Usuario editar() {
         Usuario usuario = new Usuario();
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,7 +1,19 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-
+    cargarUsuarios();
     alert(3434);
-
   $('#usuarios').DataTable();
 });
+
+async function cargarUsuarios() {
+      const request = await fetch('usuario/2345', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      });
+      const usuarios = await request.json();
+
+      console.log(usuarios);
+}
