b8130d368bdbf8bea96101af5cdc01403d189a31
Tobias <tobiasocchi03@hotmail.com>
2023-03-03 19:43:43 -0300

    added features of user sessions

---
+++ src/main/java/com/restservice/rest/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.restservice.rest.controllers;
+
+import com.restservice.rest.models.Usuario;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AuthController {
+    @RequestMapping(value = "api/login", method = RequestMethod.POST)
+    public void login(@RequestBody Usuario usuario) {
+
+        usuarioDao.verificarEmailPassword(usuario);
+    }
+
+}
--- src/main/java/com/restservice/rest/dao/UsuarioDaoImp.java
+++ src/main/java/com/restservice/rest/dao/UsuarioDaoImp.java
@@ -30,4 +30,17 @@ public class UsuarioDaoImp implements UsuarioDao{
     public void registrar(Usuario usuario) {
         entityManager.merge(usuario);
     }
+
+    @Override
+    public boolean verificarCredenciales(Usuario usuario) {
+        String query = "FROM Usuario WHERE email = :email AND password = :password";
+        List<Usuario> lista = entityManager.createQuery(query)
+                .setParameter("email",usuario.getEmail())
+                .setParameter("password",usuario.getPassword())
+                .getResultList();
+
+        return !lista.isEmpty();
+
+    }
+
 }
